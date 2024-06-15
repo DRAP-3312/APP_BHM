348fcff7fa73470e287a4afee8234da3331dfb88
David _Ake <davidake117@gmail.com>
2024-06-15 16:34:37 -0500

    RegistroLogin

--- src/users/Dto/RegistroLogin.dart
+++ src/users/Dto/RegistroLogin.dart
@@ -1,50 +1,50 @@
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsPhoneNumber,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+// import {
+//   IsEmail,
+//   IsNotEmpty,
+//   IsNumber,
+//   IsOptional,
+//   IsPhoneNumber,
+//   IsString,
+//   MaxLength,
+//   MinLength,
+// } from 'class-validator';
 
-export class CreateUserDto {
-  @IsNumber()
-  @IsOptional()
-  id?: number;
+// export class CreateUserDto {
+//   @IsNumber()
+//   @IsOptional()
+//   id?: number;
 
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(50)
-  name: string;
+//   @IsString()
+//   @IsNotEmpty()
+//   @MaxLength(50)
+//   name: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(50)
-  lastname: string;
+//   @IsString()
+//   @IsNotEmpty()
+//   @MaxLength(50)
+//   lastname: string;
 
-  @IsEmail()
-  @IsNotEmpty()
-  email: string;
+//   @IsEmail()
+//   @IsNotEmpty()
+//   email: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(13)
-  rfc: string;
+//   @IsString()
+//   @IsNotEmpty()
+//   @MaxLength(13)
+//   rfc: string;
 
-  @IsPhoneNumber('MX')
-  @IsNotEmpty()
-  @MinLength(10)
-  @MaxLength(13)
-  phone: string;
+//   @IsPhoneNumber('MX')
+//   @IsNotEmpty()
+//   @MinLength(10)
+//   @MaxLength(13)
+//   phone: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(8)
-  password: string;
+//   @IsString()
+//   @IsNotEmpty()
+//   @MinLength(8)
+//   password: string;
 
-  @IsNumber()
-  @IsNotEmpty()
-  id_bank: number;
-}
+//   @IsNumber()
+//   @IsNotEmpty()
+//   id_bank: number;
+// }
