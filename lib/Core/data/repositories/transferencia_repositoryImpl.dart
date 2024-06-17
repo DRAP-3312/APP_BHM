7c94bc9b63ea4912e71900756cae1f274d843b34
Yahir chi <yahir.chi.chi@gmail.com>
2024-06-17 00:27:49 -0500

    Mas o menos terminado

--- lib/Core/data/repositories/transferencia_repositoryImpl.dart
+++ lib/Core/data/repositories/transferencia_repositoryImpl.dart
@@ -7,11 +7,19 @@ import 'package:flutter/services.dart';
 class TransferenciaRepositoryImpl implements TransferenciaRepository {
   @override
   Future<Transferencia> loadTransferenciaData() async {
-
+    try{
     final response = await rootBundle.loadString('/assets/json_data/transferencia_data.json');
     final data = json.decode(response);
 
-    print(data);
-    return Transferencia.fromJson(data);
+    
+      print('Datos cargados del JSON: $data');
+      final miTransferencia = Transferencia.fromJson(data);
+      print('Datos convertidos a modelo: $miTransferencia');
+      return miTransferencia;
+    } catch (e) {
+      print('Error cargando los datos: $e');
+      throw Exception('Error al cargar los datos de la cuenta');
+    
   }
+}
 }
--- lib/Core/presentation/screens/PantallaTransferencia.dart
+++ lib/Core/presentation/screens/PantallaTransferencia.dart
@@ -1,10 +1,18 @@
+import 'package:bhm_app/Core/data/repositories/transferencia_repositoryImpl.dart';
+import 'package:bhm_app/Core/domain/usecases/load_transferencia_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bhm_app/Core/presentation/bloc/bloc_transferencia/transferencia_bloc.dart';
+import 'package:bhm_app/Core/presentation/bloc/bloc_transferencia/transferencia_event.dart';
+import 'package:bhm_app/Core/presentation/bloc/bloc_transferencia/transferencia_state.dart';
 import 'package:bhm_app/Core/presentation/screens/TransferScreen.dart';
 import 'package:bhm_app/service/notification.service.dart';
-import 'package:flutter/material.dart';
+
 
 // void main() {
 //   runApp(MaterialApp(home: PantallaTransferencia(usuario: 'EjemploUsuario')));
 // }
+
 
 class PantallaTransferencia extends StatefulWidget {
   final String usuario; // Parámetro requerido para la pantalla
@@ -16,99 +24,126 @@ class PantallaTransferencia extends StatefulWidget {
 }
 
 class _PantallaTransferenciaState extends State<PantallaTransferencia> {
-  String cantidad = '0';
-  String saldoDisponible = '10.000\$';
   final TextEditingController _amountController = TextEditingController();
-  String _textFieldValue = '';
   final FocusNode _focusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     _focusNode.requestFocus();
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF16697A),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const TransferScreen()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.transparent,
-                shadowColor: Colors.transparent,
-                elevation: 0,
+    return BlocProvider(
+      create: (context) => TransferenciaBloc(LoadTransferenciaData(TransferenciaRepositoryImpl()))..add(LoadTransferenciaDataEvent()),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF16697A),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const TransferScreen()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                  elevation: 0,
+                ),
+                child: const Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.white,
+                ),
               ),
-              child: const Icon(
-                Icons.arrow_back_ios,
-                color: Colors.white,
+              const Text(
+                'Cantidad a transferir',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            const Text(
-              'Cantidad a transferir',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
-                color: Colors.white,
-              ),
-            ),
-            Container(), // Puedes eliminar los contenedores adicionales si no son necesarios
-          ],
+              Container(), // Puedes eliminar los contenedores adicionales si no son necesarios
+            ],
+          ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextField(
-              controller: _amountController,
-              focusNode: _focusNode,
-              keyboardType: const TextInputType.numberWithOptions(decimal: true),
-              style: const TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-              decoration: const InputDecoration(
-                hintText: '0',
-                border: InputBorder.none,
-              ),
-              onChanged: (value) {
-                setState(() {
-                  _textFieldValue = value; // Actualizar la variable con el nuevo valor
-                });
-              },
-            ),
-            
-            
-            const SizedBox(height: 24),
-            const Text(
-              'Saldo disponible',
-              style: TextStyle(fontSize: 16.0),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 8),
-            Text(
-              saldoDisponible,
-              style:
-                  const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            const Spacer(),
-            ElevatedButton(
-              onPressed: () {
-                mostrarNotificacion(_textFieldValue, widget.usuario);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.teal,
-                foregroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(vertical: 16),
-              ),
-              child: const Text('Listo'),
-            ),
-          ],
+        body: SafeArea(
+          child: BlocBuilder<TransferenciaBloc,TranferenciaState>(
+            builder: (context, state) {
+               TextEditingController nameController = TextEditingController(text: state.nameContacto);
+              TextEditingController montoController = TextEditingController(text: state.montoTrans.toString());
+              TextEditingController fechaController = TextEditingController(text: state.fechaTrans);
+              TextEditingController saldoController = TextEditingController(text: state.saldoDisponible.toString());
+             
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextField(
+                      controller: _amountController,
+                      focusNode: _focusNode,
+                      keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                      style: const TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        hintText: '0',
+                        border: InputBorder.none,
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          // actualiza el controlador con el nuevo valor
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 24),
+                    Text(
+                      'Nombre del contacto: ${state.nameContacto}',
+                      style: const TextStyle(fontSize: 16.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Monto de la transacción: ${state.montoTrans}\$',
+                      style: const TextStyle(fontSize: 16.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Fecha de la transacción: ${state.fechaTrans}',
+                      style: const TextStyle(fontSize: 16.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 24),
+                    const Text(
+                      'Saldo disponible',
+                      style: TextStyle(fontSize: 16.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      '${state.saldoDisponible}\$',
+                      style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    const Spacer(),
+                    ElevatedButton(
+                      onPressed: () {
+                        mostrarNotificacion(_amountController.text, widget.usuario);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.teal,
+                        foregroundColor: Colors.white,
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                      ),
+                      child: const Text('Listo'),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
