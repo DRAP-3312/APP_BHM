0e0c159196eea9ad4900833240b2ab1be269d1a4
David _Ake <davidake117@gmail.com>
2024-08-05 18:32:54 -0500

    Quitar etiqueta debug

--- lib/Core/presentation/screens/LoginPage.dart
+++ lib/Core/presentation/screens/LoginPage.dart
@@ -2,45 +2,41 @@ import 'package:bhm_app/Core/data/repositories/login_repositoryImpl.dart';
 import 'package:bhm_app/Core/presentation/shared/token_stg.dart';
 import 'package:bhm_app/Core/presentation/widgets/Login.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bhm_app/Core/presentation/bloc/bloc_login/login_bloc.dart';
 import 'package:bhm_app/Core/domain/usecases/load_login_data.dart';
 import 'package:dio/dio.dart';
 
 
-void main() => runApp(const LoginPage());
-
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: BlocProvider(
-          create: (context) => LoginBloc(
-            LoadLoginData(LoginRepositoryImpl(dio: Dio(), tokenStorage :TokenStorage())),
-            LoginRepositoryImpl(dio: Dio(), tokenStorage:TokenStorage()),
-          ),
-          child: Center(
-            child: ListView(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                        margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-                        child: loginLogo()),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      child: const Login(),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+    return Scaffold(
+      appBar: AppBar(),
+      body: BlocProvider(
+        create: (context) => LoginBloc(
+          LoadLoginData(
+              LoginRepositoryImpl(dio: Dio(), tokenStorage: TokenStorage())),
+          LoginRepositoryImpl(dio: Dio(), tokenStorage: TokenStorage()),
+        ),
+        child: Center(
+          child: ListView(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                      margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                      child: loginLogo()),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: const Login(),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
